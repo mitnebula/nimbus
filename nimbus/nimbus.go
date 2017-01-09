81fd8f13fec430f8c5a3f749eefd18d5a278a7c5
Akshay Narayan <akshay.narayan@berkeley.edu>
2017-01-09 18:29:22 +0000

    profiling support

--- nimbus/nimbus.go
+++ nimbus/nimbus.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"runtime/pprof"
 )
 
 const SOCK_BUF_SIZE = 4000 * 1500
@@ -10,9 +12,20 @@ const SOCK_BUF_SIZE = 4000 * 1500
 var ip = flag.String("ip", "127.0.0.1", "IP to connect to")
 var port = flag.String("port", "42424", "Port to connect to/listen on")
 var mode = flag.String("mode", "sender", "sender or receiver")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile `file`")
 
 func main() {
 	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			fmt.Println("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			fmt.Println("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
 	fmt.Printf("%s:%s, %s\n", *ip, *port, *mode)
 
