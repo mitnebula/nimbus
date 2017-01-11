66f0435ff36e0ed36f145ddc63abfc75e509531f
Akshay Narayan <akshay.narayan@berkeley.edu>
2017-01-11 06:22:24 +0000

    convert pacing test to benchmark

--- nimbus/pace_test.go
+++ nimbus/pace_test.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"math"
 	"testing"
-	"time"
 )
 
-func TestPace(t *testing.T) {
+func BenchmarkPace(b *testing.B) {
 	pacing := make(chan interface{})
-	flowRate = 12e6
+	flowRate = 48e6
 	go flowPacer(pacing)
 
-	start := time.Now()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < b.N; i++ {
 		<-pacing
-	}
-
-	dur := time.Duration(time.Since(start).Nanoseconds()/10000) * time.Nanosecond
-	drift := time.Duration(1)*time.Millisecond - dur
-
-	if math.Abs(float64(drift.Nanoseconds())) > 500.0 {
-		t.Error("pacing drifted more than 0.5us", drift)
 	}
 }
