6d675f772d4d7c0365bef6b4412abc2c095011f8
Akshay Narayan <akshay.narayan@berkeley.edu>
2017-01-18 15:46:26 +0000

    fix receive test interface compliance

--- nimbus/receive_test.go
+++ nimbus/receive_test.go
@@ -10,14 +10,12 @@ import (
 
 type fakePacketOps struct{}
 
-func (f fakePacketOps) Listen(
-	conn *net.UDPConn,
-	res *receivedBytes,
-) {
-	hdr := bytes.Repeat([]byte{0}, 22)
-	pay := bytes.Repeat([]byte("a"), 28)
-	in := append(hdr, pay...)
-	copy(res.b, in)
+func (f fakePacketOps) SendPacket(conn *net.UDPConn, pkt Packet, size int) error {
+	return nil
+}
+
+func (f fakePacketOps) SendAck(conn *net.UDPConn, pkt Packet) error {
+	return nil
 }
 
 func (f fakePacketOps) SendRaw(
@@ -34,6 +32,30 @@ func (f fakePacketOps) SendRaw(
 	}
 
 	return nil
+}
+
+func (f fakePacketOps) RecvPacket(conn *net.UDPConn) (Packet, *net.UDPAddr, error) {
+	rcvd := receivedBytes{b: make([]byte, 1500)}
+	f.Listen(conn, &rcvd)
+	if rcvd.err != nil {
+		return Packet{}, nil, rcvd.err
+	}
+
+	return Decode(&rcvd)
+}
+
+func (f fakePacketOps) Listen(
+	conn *net.UDPConn,
+	res *receivedBytes,
+) {
+	hdr := bytes.Repeat([]byte{0}, 22)
+	pay := bytes.Repeat([]byte("a"), 28)
+	in := append(hdr, pay...)
+	copy(res.b, in)
+
+	addr, err := net.ResolveUDPAddr("udp4", ":40000")
+	res.err = err
+	res.from = addr
 }
 
 func BenchmarkReceiveLatency(b *testing.B) {
